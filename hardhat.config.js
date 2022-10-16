273befb914f8760f2d3324973d24bec2fd9af2a2
PhilippeMts <phi.metais@gmail.com>
2022-10-16 14:59:53 +0200

    🔧 Update hardhat configuration file with lotus local net.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,13 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.15",
-};
+  solidity: '0.8.15',
+  defaultNetwork: 'lotus-local-net',
+  networks: {
+    'lotus-local-net': {
+      url: 'http://localhost:1234/rpc/v0',
+      httpHeaders: {
+        'Content-Type': 'application/json',
+      },
+    },
+  },
+}
