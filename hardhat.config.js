057a463d2fe6a4994adcd68dc1bd3c429b3fd54c
PhilippeMts <phi.metais@gmail.com>
2022-10-16 17:02:01 +0200

    🧑‍💻 Set default gasLimit in the network configuration.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-require('hardhat-deploy');
+require('hardhat-deploy')
 require('@nomiclabs/hardhat-ethers')
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -12,6 +12,9 @@ module.exports = {
       httpHeaders: {
         'Content-Type': 'application/json',
       },
+      // since it's difficult to estimate the gas before f4 address is launched, it's safer to manually set
+      // a large gasLimit. This should be addressed in the following releases.
+      gas: 1000000000,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY],
     },
   },
