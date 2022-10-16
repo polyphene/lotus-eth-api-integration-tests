dcfaf87960b10bfbea392684211e839dd30b4611
PhilippeMts <phi.metais@gmail.com>
2022-10-16 17:13:59 +0200

    🚧 Add it with eth_getTransactionReceipt and eth_getTransactionByHash.

--- test/SimpleCoin.js
+++ test/SimpleCoin.js
@@ -1,14 +1,26 @@
 const { deployments, ethers } = require('hardhat')
 
+let deploymentTxHash
 let deployerF0Addr
 const otherAddress = '0xff000000000000000000000000000000deadbeef'
 
 describe('SimpleCoin', function () {
   it('Should successfully deploy', async function () {
     await deployments.fixture(['SimpleCoin'])
-    const { linkedData } = await deployments.get('SimpleCoin')
+    const { linkedData, transactionHash } = await deployments.get('SimpleCoin')
     deployerF0Addr = linkedData.f0Addr
+    deploymentTxHash = transactionHash
   })
+  it('Should access transaction details after it has been mined',
+    async function () {
+      const txByHash = await ethers.provider.getTransaction(deploymentTxHash)
+      console.log({ txByHash })
+    })
+  it('Should access transaction receipt after it has been mined',
+    async function () {
+      const txReceipt = await ethers.provider.getTransactionReceipt(deploymentTxHash)
+      console.log({ txReceipt })
+    })
   it('Should interact with the contract using eth_call', async function () {
     const SimpleCoin = await ethers.getContract('SimpleCoin')
     const deployerBalance = await SimpleCoin.getBalance(deployerF0Addr)
