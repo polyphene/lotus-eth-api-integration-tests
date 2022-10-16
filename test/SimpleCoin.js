68dc8264de4e3b6aeacafc28fc3d2ff8eca82e5b
PhilippeMts <phi.metais@gmail.com>
2022-10-16 17:08:00 +0200

    🚧 Add it with eth_call.

--- test/SimpleCoin.js
+++ test/SimpleCoin.js
@@ -1,11 +1,19 @@
-const { deployments } = require('hardhat')
+const { deployments, ethers } = require('hardhat')
 
 let deployerF0Addr
+const otherAddress = '0xff000000000000000000000000000000deadbeef'
 
 describe('SimpleCoin', function () {
   it('Should successfully deploy', async function () {
     await deployments.fixture(['SimpleCoin'])
     const { linkedData } = await deployments.get('SimpleCoin')
     deployerF0Addr = linkedData.f0Addr
+  })
+  it('Should interact with the contract using eth_call', async function () {
+    const SimpleCoin = await ethers.getContract('SimpleCoin')
+    const deployerBalance = await SimpleCoin.getBalance(deployerF0Addr)
+    console.log({ deployerBalance })
+    const receiverBalance = await SimpleCoin.getBalance(otherAddress)
+    console.log({ receiverBalance })
   })
 })
