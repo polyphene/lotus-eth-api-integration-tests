20fbc93eda0fb86653bc3c69bfcae5bcc2518887
PhilippeMts <phi.metais@gmail.com>
2022-10-17 11:58:53 +0200

    ✨ Introduce test on eth_getTransactionByHash and eth_getTransactionReceipt before the deployment transaction is mined.

--- test/SimpleCoin.js
+++ test/SimpleCoin.js
@@ -1,43 +1,80 @@
 require('dotenv').config()
-const fa = require("@glif/filecoin-address");
+const fa = require('@glif/filecoin-address')
 const { artifacts, ethers } = require('hardhat')
 const should = require('chai').should()
 
-const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
-let deployerF0Addr, deploymentTxHash, deploymentBlockHash, deploymentBlockNumber, simpleCoinAddress
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY
+let deployerF0Addr, deploymentTxHash, deploymentBlockHash,
+  deploymentBlockNumber, simpleCoinAddress
 const otherAddress = '0xff000000000000000000000000000000deadbeef'
 
 describe('SimpleCoin', function () {
-  it('Should successfully deploy', async function () {
+  it('Should send deployment transaction', async function () {
     // use the deployer private key to compute the Filecoin f1 deployer address
     // and get the right tx nonce
-    const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY);
-    const pubKey = ethers.utils.arrayify(deployer.publicKey);
-    const f1Addr = fa.newSecp256k1Address(pubKey).toString();
+    const deployer = new ethers.Wallet(DEPLOYER_PRIVATE_KEY)
+    const pubKey = ethers.utils.arrayify(deployer.publicKey)
+    const f1Addr = fa.newSecp256k1Address(pubKey).toString()
 
     try {
       // check that an actor has been deployed at the deployer address
-      let actorId = await ethers.provider.send("Filecoin.StateLookupID", [f1Addr, []]);
+      let actorId = await ethers.provider.send('Filecoin.StateLookupID',
+        [f1Addr, []])
       // format the deployer f0 address
       actorId = ethers.utils.hexValue(Number(actorId.slice(1)))
-      deployerF0Addr =  ethers.utils.hexConcat(['0xff', ethers.utils.hexZeroPad(actorId, 19)])
+      deployerF0Addr = ethers.utils.hexConcat(
+        ['0xff', ethers.utils.hexZeroPad(actorId, 19)])
     } catch (e) {
-      console.error(`failed to resolve address ${f1Addr}. be sure to deploy an actor by sending FIL there`)
+      console.error(
+        `failed to resolve address ${f1Addr}. be sure to deploy an actor by sending FIL there`)
       return
     }
 
-    const maxPriorityFeePerGas = await ethers.provider.send("eth_maxPriorityFeePerGas", [])
-    const nonce = await ethers.provider.send("Filecoin.MpoolGetNonce", [f1Addr]);
+    const maxPriorityFeePerGas = await ethers.provider.send(
+      'eth_maxPriorityFeePerGas', [])
+    const nonce = await ethers.provider.send('Filecoin.MpoolGetNonce',
+      [f1Addr])
 
-    const SimpleCoin = await ethers.getContractFactory("SimpleCoin");
-    const simpleCoin = await SimpleCoin.deploy( {
+    // create a contract factory
+    const SimpleCoin = await ethers.getContractFactory('SimpleCoin')
+    // send a deployment transaction, without waiting for the transaction to be mined
+    const simpleCoin = await SimpleCoin.deploy({
       gasLimit: 1000000000,
       maxPriorityFeePerGas,
       nonce,
-    });
+    })
 
     deploymentTxHash = simpleCoin.deployTransaction.hash
-    await simpleCoin.deployed();
+  })
+  it('Should access transaction details before it has been mined',
+    async function () {
+      const txByHash = await ethers.provider.getTransaction(deploymentTxHash)
+
+      txByHash.should.contain.keys(
+        'blockHash',
+        'blockNumber',
+        'from',
+        'hash',
+        'transactionIndex',
+      )
+      txByHash.from.should.be.a.properAddress
+      txByHash.from.should.hexEqual(deployerF0Addr)
+      should.not.exist(txByHash.blockHash)
+      should.not.exist(txByHash.blockNumber)
+      should.not.exist(txByHash.transactionIndex)
+      should.not.exist(txByHash.to)
+    })
+  it('Should access null transaction receipt before it has been mined',
+    async function () {
+      const txReceipt = await ethers.provider.getTransactionReceipt(
+        deploymentTxHash)
+
+      // eth_getTransactionReceipt returns null for both pending and unknown transactions
+      should.not.exist(txReceipt)
+    })
+  it('Should successfully deploy', async function () {
+    const tx = await ethers.provider.getTransaction(deploymentTxHash)
+    await tx.wait()
   })
   it('Should access transaction details after it has been mined',
     async function () {
@@ -127,7 +164,8 @@ describe('SimpleCoin', function () {
     blockTxCountByHash.should.be.equal(blockTxCountByNumber)
   })
   it('Should interact with the contract using eth_call', async function () {
-    const SimpleCoin = await ethers.getContractAt('SimpleCoin', simpleCoinAddress)
+    const SimpleCoin = await ethers.getContractAt('SimpleCoin',
+      simpleCoinAddress)
     const deployerBalance = await SimpleCoin.getBalance(deployerF0Addr)
     const receiverBalance = await SimpleCoin.getBalance(otherAddress)
 
@@ -136,21 +174,24 @@ describe('SimpleCoin', function () {
   })
   it('Should get the contract byte code at the deployed address',
     async function () {
-      const SimpleCoin = await ethers.getContractAt('SimpleCoin', simpleCoinAddress)
-      const code = await ethers.provider.getCode(SimpleCoin.address, "latest");
-      const { deployedBytecode } = await artifacts.readArtifact("SimpleCoin")
+      const SimpleCoin = await ethers.getContractAt('SimpleCoin',
+        simpleCoinAddress)
+      const code = await ethers.provider.getCode(SimpleCoin.address, 'latest')
+      const { deployedBytecode } = await artifacts.readArtifact('SimpleCoin')
 
       code.should.be.equal(deployedBytecode)
     })
   it('Should get storage using eth_getStorageAt', async function () {
-    const SimpleCoin = await ethers.getContractAt('SimpleCoin', simpleCoinAddress)
+    const SimpleCoin = await ethers.getContractAt('SimpleCoin',
+      simpleCoinAddress)
 
     let key = ethers.utils.hexConcat([
       ethers.utils.hexZeroPad(deployerF0Addr, 32),
       ethers.utils.hexZeroPad('0x00', 32),
     ])
     let position = ethers.utils.keccak256(key)
-    const storageAtDeployerBalance = await ethers.provider.getStorageAt(SimpleCoin.address,
+    const storageAtDeployerBalance = await ethers.provider.getStorageAt(
+      SimpleCoin.address,
       position)
 
     storageAtDeployerBalance.should.be.equal(10000)
@@ -160,7 +201,8 @@ describe('SimpleCoin', function () {
       ethers.utils.hexZeroPad('0x00', 32),
     ])
     position = ethers.utils.keccak256(key)
-    const storageAtOtherBalance = await ethers.provider.getStorageAt(SimpleCoin.address,
+    const storageAtOtherBalance = await ethers.provider.getStorageAt(
+      SimpleCoin.address,
       position)
 
     storageAtOtherBalance.should.be.equal(0)
