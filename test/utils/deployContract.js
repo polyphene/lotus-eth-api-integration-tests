dec5259444da8e042b94fa54ec183f80fc09b986
PhilippeMts <phi.metais@gmail.com>
2022-10-17 17:02:21 +0200

    🧑‍💻 DRY with some new utility functions.

---
+++ test/utils/deployContract.js
@@ -0,0 +1,22 @@
+const { ethers } = require('hardhat')
+const { getDeployerF1Address } = require('./getDeployerAddresses')
+
+const deployContract = async (contractName, ...args) => {
+  const f1Addr = getDeployerF1Address()
+
+  const maxPriorityFeePerGas = await ethers.provider.send(
+    'eth_maxPriorityFeePerGas', [])
+  const nonce = await ethers.provider.send('Filecoin.MpoolGetNonce',
+    [f1Addr])
+
+  // create a contract factory
+  const Contract = await ethers.getContractFactory(contractName)
+  // send a deployment transaction, without waiting for the transaction to be mined
+  return await Contract.deploy(...args, {
+    gasLimit: 1000000000,
+    maxPriorityFeePerGas,
+    nonce,
+  })
+}
+
+module.exports = deployContract
