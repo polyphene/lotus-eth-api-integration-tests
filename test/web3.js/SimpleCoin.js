e62b228a46f57aa3c9a415bc3203ad91855e4e0e
PhilippeMts <phi.metais@gmail.com>
2022-10-18 18:01:04 +0200

    ✨ Introduce test on eth_getTransactionByHash.

--- test/web3.js/SimpleCoin.js
+++ test/web3.js/SimpleCoin.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 const { getDeployerF1Address, getDeployerF0Address } = require(
   './utils/getDeployerAddresses')
 const deployContract = require('./utils/deployContract')
+const { web3 } = require('hardhat')
+const { promisify } = require('util')
+const should = require('chai').should()
 
-let deployerF0Addr, deploymentTxHash, pendingContract
+let deployerF0Addr, deploymentTxHash, pendingContract, deploymentBlockHash,
+  deploymentBlockNumber
 
 describe('SimpleCoin', function () {
   it('Should send deployment transaction', async function () {
@@ -18,4 +22,26 @@ describe('SimpleCoin', function () {
   it('Should successfully deploy', async function () {
     await pendingContract
   })
+  it('Should access transaction details after it has been mined',
+    async function () {
+      const txByHash = await promisify(web3.eth.getTransaction)(
+        deploymentTxHash)
+      const {
+        blockHash,
+        blockNumber,
+      } = txByHash
+      deploymentBlockHash = blockHash
+      deploymentBlockNumber = blockNumber
+
+      txByHash.should.contain.keys(
+        'blockHash',
+        'blockNumber',
+        'from',
+        'hash',
+        'transactionIndex',
+      )
+      txByHash.from.should.be.a.properAddress
+      txByHash.from.should.hexEqual(deployerF0Addr)
+      should.not.exist(txByHash.to)
+    })
 })
